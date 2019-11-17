ae6c07567d98a0359530b722a2ef3ea42a90f9df
mohannad <mohanad2021996@gmail.com>
2019-11-17 23:32:07 +0200

    Shift 2D grid

---
+++ src/Shift2DGrid.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * created by mohannad  on 17/11/19
+ */
+public class Shift2DGrid {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        while (k > 0) {
+
+            for (int i = n-1; i >=0; i--) {
+                for (int j = m-1; j >=0; j++) {
+
+                }
+            }
+            --k;
+        }
+
+        return null;
+    }
+}
