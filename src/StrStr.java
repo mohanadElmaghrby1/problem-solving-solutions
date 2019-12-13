edbe506f17213970480d92ace3e3096a400b8391
mohanadElmaghrby1 <mohanad2021996@gmail.com>
2019-12-13 15:18:57 +0200

    leetcode str str

---
+++ src/StrStr.java
@@ -0,0 +1,9 @@
+public class StrStr {
+    public static void main(String[] args) {
+
+    }
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
